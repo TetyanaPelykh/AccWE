fe52d76e8147a849817558c5a518da1fc357b594
TetyanaPelykh <tetyanapelykh@uco.edu>
2023-12-15 17:48:53 +0100

    changes1

--- src/test/java/com/example/demo/EntityUnitTest.java
+++ src/test/java/com/example/demo/EntityUnitTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -20,16 +19,16 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import com.example.demo.entities.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace=Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @TestInstance(Lifecycle.PER_CLASS)
 class EntityUnitTest {
 
-	@Autowired
-	private TestEntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
-	private Doctor d1;
+    private Doctor d1;
 
-	private Patient p1;
+    private Patient p1;
 
     private Room r1;
 
@@ -37,14 +36,62 @@ class EntityUnitTest {
     private Appointment a2;
     private Appointment a3;
 
-    @Test
-    void this_is_a_test(){
-        // DELETE THIS TEST
-        assertThat(false).isEqualTo(true);
+    @BeforeAll
+    void setUp() {
+        
+        d1 = new Doctor("John", "Doe", 35, "john.doe@example.com");
+        p1 = new Patient("Jane", "Doe", 25, "jane.doe@example.com");
+        r1 = new Room("Dermatology");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+        LocalDateTime startsAt1 = LocalDateTime.parse("10:00 01/01/2023", formatter);
+        LocalDateTime finishesAt1 = LocalDateTime.parse("11:00 01/01/2023", formatter);
+
+        LocalDateTime startsAt2 = LocalDateTime.parse("14:00 01/01/2023", formatter);
+        LocalDateTime finishesAt2 = LocalDateTime.parse("15:00 01/01/2023", formatter);
+
+        LocalDateTime startsAt3 = LocalDateTime.parse("16:00 01/01/2023", formatter);
+        LocalDateTime finishesAt3 = LocalDateTime.parse("17:00 01/01/2023", formatter);
+
+        a1 = new Appointment(p1, d1, r1, startsAt1, finishesAt1);
+        a2 = new Appointment(p1, d1, r1, startsAt2, finishesAt2);
+        a3 = new Appointment(p1, d1, r1, startsAt3, finishesAt3);
     }
 
-    /** TODO
-     * Implement tests for each Entity class: Doctor, Patient, Room and Appointment.
-     * Make sure you are as exhaustive as possible. Coverage is checked ;)
-     */
+    @Test
+    void testDoctorEntity() {
+        
+        Doctor savedDoctor = entityManager.persistAndFlush(d1);
+        Doctor retrievedDoctor = entityManager.find(Doctor.class, savedDoctor.getId());
+
+        assertThat(retrievedDoctor).isEqualTo(d1);
+    }
+
+    @Test
+    void testPatientEntity() {
+        Patient savedPatient = entityManager.persistAndFlush(p1);
+        Patient retrievedPatient = entityManager.find(Patient.class, savedPatient.getId());
+
+        assertThat(retrievedPatient).isEqualTo(p1);
+    }
+
+    @Test
+    void testRoomEntity() {
+        // Aquí puedes escribir pruebas específicas para la entidad Room
+        Room savedRoom = entityManager.persistAndFlush(r1);
+        Room retrievedRoom = entityManager.find(Room.class, savedRoom.getRoomName());
+
+        assertThat(retrievedRoom).isEqualTo(r1);
+    }
+
+    @Test
+    void testAppointmentEntity() {
+        // Aquí puedes escribir pruebas específicas para la entidad Appointment
+        Appointment savedAppointment = entityManager.persistAndFlush(a1);
+        Appointment retrievedAppointment = entityManager.find(Appointment.class, savedAppointment.getId());
+
+        assertThat(retrievedAppointment).isEqualTo(a1);
+    }
 }
+
